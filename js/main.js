79df322ca39e686b831eb5f4045abf10f21169ee
Ahmed Essam <>
2020-11-14 20:13:48 +0200

    fix mixitup

--- js/main.js
+++ js/main.js
@@ -3,52 +3,44 @@ $(document).ready(function () {
     $('#main-slide').carousel({
         interval: 7000
     });
+
+
     $(window).on('scroll', function () {
         if ($(this).scrollTop() >= 250) {
             $('.main-bar').addClass('navbar-fixed-top');
+            $('.catigory span').animate({
+                top: '0'
+            }, 1);
         } else {
             $('.main-bar').removeClass('navbar-fixed-top');
-        }
+            $('.catigory span').animate({
+                top: '-7px'
+            }, 1);
+        };
     });
 
     // Start Mixer 1
-    var containerEl;
-    var mixer = mixitup('#filtery1');
-    var mixer = mixitup('#filtery1', {
-        multifilter: {
-            enable: true // enable the multifilter extension for the mixer
+    var containerEl1 = document.querySelector('[data-ref="container-1"]');
+    var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+    var config1 = {
+        controls: {
+            scope: 'local'
         },
-        selectors: {
-            target: '.blog-item',
-            filter: '.btn-filter1'
-        },
-        animation: {
-            duration: 300
+        load: {
+            filter: '.latest'
         }
-    });
-    mixer.filter('.latest');
-
-    // Start swiper2 
-    $('#latest-slide, #feature-slide, #best-slide').carousel({
-        interval: false
-    });
-    // =====================================================
-
-    var containerEl;
-    var mixer = mixitup('#filtery2');
-    var mixer = mixitup('#filtery2', {
-        multifilter: {enable: true}, // enable the multifilter extension for the mixer
-        selectors: {
-            target: '.blog-item',
-            filter: '.btn-filter2'
+    };
+    var config2 = {
+        controls: {
+            scope: 'local'
         },
-        animation: {
-            duration: 300
+        load: {
+            filter: '.care'
         }
-    });
-    mixer.filter('.care');
+    };
 
-    $('#care-slide, #tool-slide, #mobi-slide').carousel({
-        interval: false
-    });
+    var mixer1 = mixitup(containerEl1, config1);
+    var mixer1 = mixitup(containerEl2, config2);
+
 });
